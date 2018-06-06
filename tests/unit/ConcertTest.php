7d4bbbac038a4efef5367657dae875bfa1c1acc5
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-06 14:15:08 +0300

    Aded accessors for concert

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -42,7 +42,7 @@ class ConcertTest extends TestCase
     /** @test */
     function can_get_formatted_date()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 8:00pm'),
         ]);
 
@@ -52,11 +52,21 @@ class ConcertTest extends TestCase
     /** @test */
     function can_get_formatted_start_time()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([
             'date' => Carbon::parse('2016-12-01 17:00:00'),
         ]);
 
         $this->assertEquals('5:00pm', $concert->formatted_start_time);
+    }
+
+    /** @test */
+    function can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750,
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
 

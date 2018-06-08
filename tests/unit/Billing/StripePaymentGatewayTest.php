4b47285b0adc63224307529c5b5d35dc41f7dc6c
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-08 10:44:04 +0300

    Made a successful charge

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -6,16 +6,17 @@
  * Time: 9:47 AM
  */
 
+use App\Billing\StripePaymentGateway;
 class StripePaymentGatewayTest extends TestCase
 {
     /** @test */
     function charges_with_a_valid_payment_token_are_successful()
     {
-        $paymentGateway = new StripePaymentGatewayTest;
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
         $token = \Stripe\Token::create([
             "card" => [
-                "number" => "42424242424242",
+                "number" => "4242424242424242",
                 "exp_month" => 1,
                 "exp_year" => date('Y') + 1,
                 "cvc" => "123"
@@ -23,5 +24,12 @@ class StripePaymentGatewayTest extends TestCase
         ], ['api_key' => config('services.stripe.secret')])->id;
 
         $paymentGateway->charge(2500, $token);
+
+        $lastCharge = \Stripe\Charge::all(
+            ['limit' => 1],
+            ['api_key' => config('services.stripe.secret')]
+        )['data'][0];
+
+        $this->assertEquals(2500, $lastCharge->amount);
     }
 }

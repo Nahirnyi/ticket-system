a6026762c6aee2b6a4def084ab4282407aa6e294
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-08 12:58:54 +0300

    Making the tests identical

--- app/Billing/FakePaymentGateway.php
+++ app/Billing/FakePaymentGateway.php
@@ -48,4 +48,11 @@ class FakePaymentGateway implements PaymentGateway
     {
         $this->beforeFirstChargeCallback = $callback;
     }
+
+    public function newChargesDuring($callback)
+    {
+        $chargesFrom = $this->charges->count();
+        $callback($this);
+        return $this->charges->slice($chargesFrom)->values();
+    }
 }
--- app/Billing/PaymentGateway.php
+++ app/Billing/PaymentGateway.php
@@ -14,4 +14,6 @@ interface PaymentGateway
     public function charge($amount, $token);
 
     public function getValidTestToken();
+
+    public function newChargesDuring($callback);
 }

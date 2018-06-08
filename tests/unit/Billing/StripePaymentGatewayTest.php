48f5491cb5b2cdacf3e24468067a6fb5cdcc5e4e
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-08 11:30:57 +0300

    Handling invalid payment tokens

--- tests/unit/Billing/StripePaymentGatewayTest.php
+++ tests/unit/Billing/StripePaymentGatewayTest.php
@@ -7,6 +7,12 @@
  */
 
 use App\Billing\StripePaymentGateway;
+use App\Billing\PaymentFailedException;
+
+/**
+ * Class StripePaymentGatewayTest
+ * @group integration
+ */
 class StripePaymentGatewayTest extends TestCase
 {
     private function lastCharge()
@@ -56,5 +62,21 @@ class StripePaymentGatewayTest extends TestCase
 
         $this->assertCount(1, $this->newCharges());
         $this->assertEquals(2500, $this->lastCharge()->amount);
+    }
+
+    /** @test */
+    function charges_with_an_invalid_payment_token_fail()
+    {
+        try{
+            $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
+
+            $paymentGateway->charge(2500, 'invalid-payment-token');
+        } catch (PaymentFailedException $e){
+            $this->assertCount(0, $this->newCharges());
+            return;
+        }
+
+        $this->fail("Charging with an invalid token did not throw a PaymentFailedExeption");
+
     }
 }

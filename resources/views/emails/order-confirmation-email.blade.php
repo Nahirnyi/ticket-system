9921b2cc8463c891a05a967da9ca482b938346d9
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-12 11:24:42 +0300

    Testing mailable Contents

---
+++ resources/views/emails/order-confirmation-email.blade.php
@@ -0,0 +1,5 @@
+<p>Thank you for your order</p>
+
+<p>View your tickets</p>
+
+<a href="{{ url("/orders/{$order->confirmation_number}") }}">{{ url("/orders/{$order->confirmation_number}") }}</a>
---
+++ tests/unit/Mail/OrderConfirmationEmailTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 6/12/18
+ * Time: 11:02 AM
+ */
+
+use App\Order;
+use App\Mail\OrderConfirmationEmail;
+
+class OrderConfirmationEmailTest extends TestCase
+{
+    /** @test */
+    function email_contains_a_link_to_the_order_confirmation_test()
+    {
+        $order = factory(Order::class)->make([
+            'confirmation_number' => 'ORDERCONFIRMATION1234'
+        ]);
+
+        $email = new OrderConfirmationEmail($order);
+        // IN Laravel 5.5
+        //$rendered = $email->render()
+
+        $rendered = $this->render($email);
+        $this->assertContains(url('/orders/ORDERCONFIRMATION1234'), $rendered);
+    }
+
+    private function render($mailable)
+    {
+        $mailable->build();
+        return view($mailable->view, $mailable->buildViewData())->render();
+    }
+
+    /** @test */
+    function email_has_a_subject()
+    {
+        $order = factory(Order::class)->make();
+        $email = new OrderConfirmationEmail($order);
+        $this->assertEquals('Your TicketBeast Order', $email->build()->subject);
+    }
+}

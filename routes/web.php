c4fa343741b6d389addccf9c9296535498bd2d03
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-12 15:31:47 +0300

    Getting started with laravel dusk

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function (){
+    return 'Laravel';
+});
 
 Route::get('/concerts/{id}', 'ConcertsController@show');
 

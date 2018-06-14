5a68d2cd3fe96e76a3bdcb892e61173e1761445f
User TenantCloud <user@Users-MacBook-Pro.local>
2018-06-14 10:12:02 +0300

    Creating tickets at time of publish

--- tests/Feature/Backstage/AddConcerTest.php
+++ tests/Feature/Backstage/AddConcerTest.php
@@ -99,6 +99,7 @@ class AddConcerTest extends TestCase
             $this->assertEquals('12345', $concert->zip);
             $this->assertEquals(3250, $concert->ticket_price);
             $this->assertEquals(75, $concert->ticketsRemaining());
+            $this->assertEquals(75, $concert->ticket_quantity);
          });
 
     }
